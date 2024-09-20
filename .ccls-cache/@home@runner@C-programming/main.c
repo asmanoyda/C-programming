567c0f51841bbbd832080d1c72b61920f9b8d225
Asma <69631723+asmanoyda@users.noreply.github.com>
2024-09-20 08:58:15 +0000

    Area of rectangle

--- .ccls-cache/@home@runner@C-programming/main.c
+++ .ccls-cache/@home@runner@C-programming/main.c
@@ -7,19 +7,28 @@ void variables() {
   printf("value of float is = %f\n", b);
   printf("value of charcter is = %c\n", c);
 }
-void arithmetic(){
-  int a = 100,b = 200,c = 300;
-  printf("sum of a and b is = %d\n",a+b);
-  printf("subtraction of c and b is = %d\n",c-b);
-  printf("multiplication of a and b is = %d\n",a*b);
-  printf("division of c and a is = %d\n",c/a);
+void arithmetic() {
+  int a = 100, b = 200, c = 300;
+  printf("sum of a and b is = %d\n", a + b);
+  printf("subtraction of c and b is = %d\n", c - b);
+  printf("multiplication of a and b is = %d\n", a * b);
+  printf("division of c and a is = %d\n", c / a);
+}
+// program to find area of rectangle 
+void rectangle() {
+ int length =10 , width =20, area;
+  area = length* width;
+  printf("area of rectangle is = %d",area);
+  
   
 }
+
 
 int main() {
   printf("Hello World\n");
   printf("C language learning\n");
-  //variables();
-  arithmetic();
+  // variables();
+  rectangle();
+  //arithmetic();
   return 0;
 }
