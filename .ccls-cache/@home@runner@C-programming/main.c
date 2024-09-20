fc27880a07a0f82e1eb0c0fcafdbf3f668239f36
Asma <69631723+asmanoyda@users.noreply.github.com>
2024-09-20 08:49:41 +0000

    Arithmetic operations

--- .ccls-cache/@home@runner@C-programming/main.c
+++ .ccls-cache/@home@runner@C-programming/main.c
@@ -7,10 +7,19 @@ void variables() {
   printf("value of float is = %f\n", b);
   printf("value of charcter is = %c\n", c);
 }
+void arithmetic(){
+  int a = 100,b = 200,c = 300;
+  printf("sum of a and b is = %d\n",a+b);
+  printf("subtraction of c and b is = %d\n",c-b);
+  printf("multiplication of a and b is = %d\n",a*b);
+  printf("division of c and a is = %d\n",c/a);
+  
+}
 
 int main() {
   printf("Hello World\n");
   printf("C language learning\n");
-  variables();
+  //variables();
+  arithmetic();
   return 0;
 }
