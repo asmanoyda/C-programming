7eaf1e80e62f0eaa948b8464e6b6a6045f333a87
Asma <69631723+asmanoyda@users.noreply.github.com>
2024-09-20 05:49:47 +0000

    Variables

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+void variables() {
+  int a = 10;
+  float b = 3.14;
+  char c = 'A';
+  printf("value of integer is  = %d\n", a);
+  printf("value of float is = %f\n", b);
+  printf("value of charcter is = %c\n", c);
+}
 
-int main(void) {
+int main() {
   printf("Hello World\n");
-  printf("C language learning ");
+  printf("C language learning\n");
+  variables();
   return 0;
 }
